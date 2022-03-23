4bff6357dc2226b07a92cefe60c2c855ecbc5ec4
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-03-23 11:00:57 +0000

    Update client.c

--- P2/super/client.c
+++ P2/super/client.c
@@ -23,16 +23,13 @@ so that he can draw them os the window */
 paddle_position_type paddles[MAX_PLAYERS];
 ball_position_type ball;
 
-
 /* client has to know which ID belongs to him */
 int client_id;
-
 
 /* client program lauches this thread that keeps on listening for server messages until
 the client disconects from the server. in practice, the only type of messages that can be 
 received are BOARD_UPDATE's so the client thread has to refresh de ball, the paddles and the
 scoreboard by updating the new values for each one (they came inside de server message) */
-
 void comunication_thread(void *arg)
 {
     int server_socket = *(int*) arg;
@@ -101,24 +98,20 @@ void comunication_thread(void *arg)
     }
 }
 
-
 /* client main method starts by establishing a connection with the socket and them launches
 a thread that keep listening the server messages. in the main method the user is allow to 
 use the keyboard for playing or for disconecting the server. each time the user presses one valid 
 key for moving the paddle, a PADDLE_UPDATE is send to server */
-
 int main (int argc, char *argv[])
 {
     // validate serve address
     if (argc < 2)
         error("arg[1]");
 
-
     // socket
     int socket_main = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_main == -1)
         error("socket");
-
 
     // configure socket
     struct sockaddr_in server_address;
@@ -126,17 +119,14 @@ int main (int argc, char *argv[])
     server_address.sin_addr.s_addr = INADDR_ANY;    
     server_address.sin_port = htons(SOCK_PORT);    
 
-
     // send CONNECT
     int op = connect(socket_main, (struct sockaddr*) &server_address, sizeof(server_address));
     if (op == -1)
         error("connect");
 
-
     // server comunication thread
     pthread_t server_t_id;
     pthread_create(&server_t_id, NULL, comunication_thread, &socket_main); 
-
 
     //begin game
     initscr();		    	
@@ -153,15 +143,12 @@ int main (int argc, char *argv[])
     box(message_win, 0 , 0);	
     wrefresh(message_win);
 
-
     // initial ball position
     place_ball_random(&ball);
 
-
-    //setup
+   //setup
     message msg_client;
     int key;
-
 
     // until the user disconnects, keeps playing
     while(1)
@@ -171,7 +158,6 @@ int main (int argc, char *argv[])
         // playing            
         if ((key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN))
         {             
-
             msg_client.type = PADDLE_MOVE;
             msg_client.move = key;
             msg_client.id = client_id;
@@ -187,7 +173,6 @@ int main (int argc, char *argv[])
             op == sendto(socket_main, &msg_client, sizeof(msg_client), 0, (struct sockaddr *) &server_address, sizeof(server_address));  
             if (op == -1)
                 error("server unavailable");
-
         }    
         // disconnect
         else if (key = 'q')
